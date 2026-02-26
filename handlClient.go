6cfffde2f49896a820dfc025cc324c04d882fa95
otalhaou <oumatlh4@gmail.com>
2026-02-26 21:36:37 +0000

    Add Cmnts

--- handlClient.go
+++ handlClient.go
@@ -12,60 +12,66 @@ import (
 var (
 	clients     = make(map[string]net.Conn)
 	historiques []string
-	mu          sync.Mutex
-	form        string
+	formTime    string
+	mu sync.Mutex
 )
 
 func HandlClient(con net.Conn) {
 	defer con.Close()
 
-	buf := bufio.NewReader(con)
-
+	//Welcome => Client
 	welcome, err := os.ReadFile("welcome.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	con.Write(welcome)
 
-	nameInput, _ := buf.ReadString('\n')
+	buf := bufio.NewReader(con)
 
+	//Gestion d'erreur => Entrez votre nom (Doublons d nom + Ascii pour le nom + nom est vide)
+	name := ""
 	for {
-		nameInput, _ = buf.ReadString('\n')
+		nameInput, _ := buf.ReadString('\n')
 
-		if len(nameInput) == 0 {
+		if nameInput == "\n" {
 			con.Write([]byte("[ENTER YOUR NAME]:"))
 			continue
 		}
 
+		check := false
 		for _, r := range nameInput {
-			if r < 32 || r > 126 {
+			if (r < 32 || r > 126) && r != '\n' {
 				con.Write([]byte("[ENTER YOUR NAME]:"))
-				continue
+				check = true
+				break
 			}
 		}
-		mu.Lock()
-		_, exists := clients[nameInput]
-		mu.Unlock()
+		if check {
+			continue
+		}
+
+		_, exists := clients[nameInput[:len(nameInput)-1]]
 		if exists {
 			con.Write([]byte("[NAME ALREADY EXISTS]\n"))
 			con.Write([]byte("[ENTER YOUR NAME]:"))
-
 			continue
 		}
-
+		name = nameInput[:len(nameInput)-1]
 		break
 	}
 
-	name := nameInput[:len(nameInput)-1]
+//Implementez map avec client courant 
 	mu.Lock()
 	clients[name] = con
 	mu.Unlock()
 
 	now := time.Now()
-	form = now.Format("[2006-01-02 15:04:05]")
+	formTime = now.Format("[2006-01-02 15:04:05]")
 
+//Serveur sert a Distribué au clients =>  .. has joined our chat
 	BroadCast(con, name+" has joined our chat\n")
 
+//Serveur sert a Distribué l'historique s'ils existent pour le client courant 
 	for _, history := range historiques {
 		for _, c := range clients {
 			if c == con {
@@ -74,8 +80,9 @@ func HandlClient(con net.Conn) {
 		}
 	}
 
+//le Coeur de Serveur => hna  fach knjme3 les msg o nhethoum f historiques o serveru y distribuyehum les autres clients 
 	for {
-		ligne := form + "[" + name + "]:"
+		ligne := formTime + "[" + name + "]:"
 		con.Write([]byte(ligne))
 
 		messageContent, err := buf.ReadString('\n')
@@ -86,7 +93,6 @@ func HandlClient(con net.Conn) {
 			mu.Unlock()
 			return
 		}
-
 		found := false
 		for _, r := range messageContent {
 			if (r < 32 || r > 126) && r != '\n' {
@@ -106,10 +112,12 @@ func HandlClient(con net.Conn) {
 	}
 }
 
+
+// o HADA BRODCAT li kayseft ge3 l msg dyl client courant l es autres clients 
 func BroadCast(con net.Conn, msg string) {
 	for n, c := range clients {
 		if c != con {
-			c.Write([]byte("\n" + msg + form + "[" + n + "]:"))
+			c.Write([]byte("\n" + msg + formTime + "[" + n + "]:"))
 		}
 	}
 }
